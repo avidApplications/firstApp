a3c774932e106aaf8d58b6ba55e4d1795d3e3573
Bradley Stovall <bradleystovall@Stovalls-MBA.local>
2017-03-08 09:08:15 -0900

    My first commit.

--- My First App/ViewController.swift
+++ My First App/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonTapped(sender: AnyObject) {
+        self.view.backgroundColor = UIColor.redColor()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
